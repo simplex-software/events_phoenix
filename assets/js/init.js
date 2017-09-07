25cbe6b128209e5fc80887fc568063ddd5c7c08b
Francisco Rossi <francisco.rossi@simplex.software>
2017-09-07 10:46:08 -0300

    adding event list page

---
+++ assets/js/init.js
@@ -0,0 +1,7 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('ul.tabs').tabs({ 'swipeable': true });
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
--- lib/events_web/views/event_view.ex
+++ lib/events_web/views/event_view.ex
@@ -1,3 +1,9 @@
 defmodule EventsWeb.EventView do
   use EventsWeb, :view
+  alias Events.Social.Event
+  alias Events.Social
+
+  def count_participants(event = %Event{}) do
+      Social.get_participants_count(event.id)
+  end
 end
